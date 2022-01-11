078957af3e5be9acf21f528a28e90562afd863e4
AreYouScorpio <akos.schweger@gmail.com>
2022-01-12 00:43:26 +0100

    Running feladat beolvasás kész

---
+++ src/main/java/day01/Run.java
@@ -0,0 +1,32 @@
+package day01;
+
+import java.time.LocalDate;
+
+public class Run {
+    double km;
+    LocalDate date;
+
+    public Run(double km, LocalDate date) {
+        this.km = km;
+        this.date = date;
+    }
+
+    public Run(String line) {
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/day01/Running.java
@@ -0,0 +1,42 @@
+package day01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Running {
+
+    public static final String SEPARATOR = ";";
+
+    private String values;
+    private Path fileName;
+
+    public Running(Path fileName) {
+        this.fileName = fileName;
+    }
+
+    public List<Run> runnings() {
+        List<Run> runnings = new ArrayList<>();
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName.toString()))) {
+            while ((line = reader.readLine()) != null) {
+                Run run = new Run(line);
+                runnings.add(run);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Error by parsing, general io", ioe);
+        }
+        return runnings;
+    }
+
+    public static void main(String[] args) {
+        Running running = new Running(Paths.get("src/main/resources/running.csv"));
+        System.out.println(running.runnings());
+
+    }
+}
