8953399c821442d225a606341c714b8a5bd10764
AreYouScorpio <akos.schweger@gmail.com>
2022-01-12 11:15:07 +0100

    catalog project task working on it..

--- src/main/java/catalog/AudioFeatures.java
+++ src/main/java/catalog/AudioFeatures.java
@@ -1,4 +1,44 @@
 package catalog;
 
-public class AudioFeatures {
+import java.util.List;
+
+public class AudioFeatures implements Feature {
+    public List<String> getComposer() {
+        return composer;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public List<String> getPerformers() {
+        return performers;
+    }
+
+    List<String> composer;
+    int length;
+    List<String> performers;
+    String title;
+
+    public AudioFeatures (String title, int length, List<String> performers) {
+
+    }
+
+    public AudioFeatures (String title, int length, List<String> performers, List<String> composer) {
+
+    }
+
+    @Override
+    public List<String> getContributors() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+
+
+
+
 }
--- src/main/java/catalog/PrintedFeatures.java
+++ src/main/java/catalog/PrintedFeatures.java
@@ -1,4 +1,20 @@
 package catalog;
 
-public class PrintedFeatures {
+import java.util.List;
+
+public class PrintedFeatures implements Feature {
+
+    List<String> authors;
+    int numberOfPages;
+    String title;
+
+    @Override
+    public List<String> getContributors() {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
 }
