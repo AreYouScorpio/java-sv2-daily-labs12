82048e2bf19da2acbb6b33ef9d32175898a6c4e4
AreYouScorpio <akos.schweger@gmail.com>
2022-01-12 13:27:12 +0100

    catalog project task .. catalogItem working

--- src/main/java/catalog/CatalogItem.java
+++ src/main/java/catalog/CatalogItem.java
@@ -1,4 +1,34 @@
 package catalog;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CatalogItem {
+
+    List<Feature> features = new ArrayList<>();
+    int price;
+    String registrationNumber;
+
+    public CatalogItem(String registrationNumber, int price, List<Feature> features ) {
+        this.features = features;
+        this.price = price;
+        this.registrationNumber = registrationNumber;
+    }
+
+
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+
+
 }
