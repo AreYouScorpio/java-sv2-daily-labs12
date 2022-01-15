9692132785b5a86b57822fa551cccb4e5d6ca347
AreYouScorpio <akos.schweger@gmail.com>
2022-01-15 12:15:19 +0100

    day05 charCalculator Map<> task done

--- src/main/java/day05/StringStatistics.java
+++ src/main/java/day05/StringStatistics.java
@@ -1,41 +1,36 @@
 package day05;
 
-import javax.swing.plaf.IconUIResource;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class StringStatistics {
 
-    private Map<Character, Integer> howManyCharacterIsInTheString(String input){
+    private Map<Character, Integer> howManyCharacterIsInTheString(String input) {
 
         Map<Character, Integer> map = new LinkedHashMap<>();
 
-        int counter=0;
-
         char[] ch = new char[input.length()];
 
-        for (int i=0; i<ch.length; i++) {
+        for (int i = 0; i < ch.length; i++) {
             ch[i] = input.charAt(i);
         }
 
-        for (int i=0; i< ch.length; i++){
+        for (int i = 0; i < ch.length; i++) {
             if (map.containsKey(ch[i])) {
-                System.out.println(map.get(ch[i])+"hello");
-                counter=map.get(ch[i])+1;
-                map.put(ch[i], counter);
+                System.out.println(map.get(ch[i]) + "hello");
+                map.put(ch[i], map.get(ch[i]) + 1);
             } else {
-                map.put(ch[i],1);
+                map.put(ch[i], 1);
             }
         }
         System.out.println(map);
         return map;
-        }
+    }
 
 
     public static void main(String[] args) {
         StringStatistics stringStatistics = new StringStatistics();
-        stringStatistics.howManyCharacterIsInTheString("sszzzzziasi");
+        stringStatistics.howManyCharacterIsInTheString("sszzzzziasix");
     }
 
 }
